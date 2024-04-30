21654900321bfc98335438b5d0c77ac4263cb73a
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-04-30 21:27:15 +0900

    navigation, datepicker 구현

--- prototype/src/screens/Onboard/onboard-1.js
+++ prototype/src/screens/Onboard/onboard-1.js
@@ -9,22 +9,22 @@ import { setColor } from '../../stores/color-slice';
 import { plusNumber } from '../../stores/test-slice';
 import clock from '../../assets/onboard1/clock.png'
 import FullSizeButton from '../../component/ui/buttons/full-size-button';
+import { useNavigation } from '@react-navigation/native';
+
+
 export default function Onboard_1() {
+    const navigation = useNavigation();
 
     const color = useSelector((state) => state.color.value);
     const number = useSelector((state) =>state.number.value)
 
-    console.log(color)
-
     const dispatch = useDispatch();
-    const Hibtn=()=>{
-        console.log('hi')
-    }
+
     return(
         <View style={[styles.continer, {backgroundColor:color}]}>
             <RegularText style={styles.script}>늦지 않게 해드릴게요!</RegularText>
             <Button onPress={() => dispatch(setColor())} title='이걸 누르면 색깔이 막 바뀌어용!'></Button>
-            <FullSizeButton onPress={Hibtn}/>
+            <FullSizeButton onPress={() => navigation.navigate('Onboard_2')} children="시작하기"/>
         </View>
     )
 }
