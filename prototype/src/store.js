88d94371faf53096ec204dc7caa18c6d120f7cd9
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-05-01 15:58:09 +0900

    Time Redux, 격려 페이지 구현

--- prototype/src/store.js
+++ prototype/src/store.js
@@ -2,11 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import colorSlice from './stores/color-slice';
 import testSlice from './stores/test-slice';
+import timeReducer from './stores/time-select-slice';
 
 export const store = configureStore({
     reducer: {
         color : colorSlice,
-        number : testSlice
+        number : testSlice,
+        time: timeReducer,
     },
     
 });
