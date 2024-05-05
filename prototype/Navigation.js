ff190e300d559a8d26573e6b2dc11c4eace5ce95
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-05-05 16:01:38 +0900

    씻기 페이지 animated background, time controll, success fail 모달 구현

--- prototype/Navigation.js
+++ prototype/Navigation.js
@@ -7,6 +7,7 @@ import Onboard_1 from "./src/screens/Onboard/onboard-1";
 import Onboard_2 from "./src/screens/Onboard/onboard-2";
 import Praise from "./src/screens/Onboard/praise";
 import Disappoint from "./src/screens/Onboard/disappoint";
+import Shower from "./src/screens/Onboard/Shower";
 
 const Stack = createStackNavigator();
 
@@ -18,6 +19,7 @@ function Navigation(){
                 <Stack.Screen options={{ headerShown: false }} name="Onboard_2" component={Onboard_2}/>
                 <Stack.Screen options={{ headerShown: false }} name="Praise" component={Praise}/>
                 <Stack.Screen options={{ headerShown: false }} name="Disappoint" component={Disappoint}/>
+                <Stack.Screen options={{ headerShown: false }} name="Shower" component={Shower}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
